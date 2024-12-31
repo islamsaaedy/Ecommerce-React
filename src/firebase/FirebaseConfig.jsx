db403e4c53b7c0aa0568a7a22cc2acd359a520d1
Eslam Saeed Mohamed <102478392+islamsaaedy@users.noreply.github.com>
2024-12-31 12:18:21 +0200

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -3,16 +3,18 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIxvP9B_ZHCM33tkLCCLJnCeSux9PJcGg",
-  authDomain: "shop-84025.firebaseapp.com",
-  projectId: "shop-84025",
-  storageBucket: "shop-84025.firebasestorage.app",
-  messagingSenderId: "987073019140",
-  appId: "1:987073019140:web:67a8f0e34b69cf95d1a451",
+  apiKey: "AIzaSyXXXXXXX_EXAMPLE_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app-id",
+  storageBucket: "your-app-storage.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:exampleappid12345",
 };
+
 const app = initializeApp(firebaseConfig);
 
 const fireDB = getFirestore(app);
 const auth = getAuth(app);
 
 export { fireDB, auth };
+
